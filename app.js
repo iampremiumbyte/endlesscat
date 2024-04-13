034683a8446e4da2cbb0f6498ed5e65d6b62bc1f
Godwin Idemudia <godwinide@gmail.com>
2024-04-13 18:05:42 +0100

    furst commit

--- app.js
+++ app.js
@@ -1,40 +1,48 @@
 
 window.addEventListener('scroll', function () {
-    if ((this.window.scrollY * 2) >= this.window.innerHeight) {
+    if ((this.window.scrollY * 2.1) >= this.window.innerHeight) {
         // Create a pre element
         const preElement = document.createElement('pre');
 
         // Add some text content to the pre element
         const text = document.createTextNode(
             `
-| |   | |
-| |   | |
-| |   | |
-| |   | |
-| |   | |
-| |   | |
-| |   | |
-| |   | |
-| |   | |
-| |   | |
-| |   | |
-| |   | |
-| |   | |
-| |   | |
-| |   | |
-| |   | |
-| |   | |
-| |   | |
-| |   | |
-| |   | |
-| |   | |
-| |   | |
-| |   | |
-| |   | |
-| |   | |
-| |   | |
-| |   | |
-| |   | |
+ | |   | |
+ | |   | |
+ | |   | |
+ | |   | |
+ | |   | |
+ | |   | |
+ | |   | |
+ | |   | |
+ | |   | |
+ | |   | |
+ | |   | |
+ | |   | |
+ | |   | |
+ | |   | |
+ | |   | |
+ | |   | |
+ | |   | |
+ | |   | |
+ | |   | |
+ | |   | |
+ | |   | |
+ | |   | |
+ | |   | |
+ | |   | |
+ | |   | |
+ | |   | |
+ | |   | |
+ | |   | |
+ | |   | |
+ | |   | |
+ | |   | |
+ | |   | |
+ | |   | |
+ | |   | |
+ | |   | |
+ | |   | |
         `
         );
         preElement.appendChild(text);
